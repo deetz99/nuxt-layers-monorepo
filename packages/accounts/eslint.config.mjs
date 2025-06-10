b7395d77030a34e547093e56ea7177edc548d851
deetz99 <dietrich@wolpert.ca>
2025-06-10 14:46:54 -0700

    initial layer directories

---
+++ packages/accounts/eslint.config.mjs
@@ -0,0 +1,7 @@
+import withNuxt from './.playground/.nuxt/eslint.config.mjs'
+import config from '../../eslint-defaults.js'
+
+export default withNuxt({
+  ...config
+})
+
---
+++ packages/accounts/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+
+  devtools: { enabled: false },
+
+  compatibilityDate: '2025-06-10',
+
+  future: {
+    compatibilityVersion: 4
+  }
+})
