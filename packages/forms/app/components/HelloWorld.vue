679017c67eec7f630b50921c9deca0780d39ea29
deetz99 <dietrich@wolpert.ca>
2025-07-10 10:45:57 -0700

    added forms layer

---
+++ packages/forms/app/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { myLayer } = useAppConfig()
+</script>
+
+<template>
+  <div>
+    <h1>Hello World! Changed Again Again Again Again!</h1>
+    <pre>{{ myLayer }}</pre>
+  </div>
+</template>
