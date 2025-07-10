095bba9e7409deb9ed56f416e87fb71402cfa285
deetz99 <dietrich@wolpert.ca>
2025-07-10 16:00:08 -0700

    remove compatibility version from all demo apps

---
+++ .changeset/great-places-give.md
@@ -0,0 +1,8 @@
+---
+"accounts-layer-demo-app": patch
+"forms-layer-demo-app": patch
+"base-layer-demo-app": patch
+"pay-layer-demo-app": patch
+---
+
+Remove compatibility version from all demo apps
--- apps/demo/forms/nuxt.config.ts
+++ apps/demo/forms/nuxt.config.ts
@@ -3,9 +3,6 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
-  future: {
-    compatibilityVersion: 4
-  },
   modules: [
     '@nuxt/eslint'
   ],
