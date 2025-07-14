6c0f2765f69c6d3979c9e9a32c4097f59794cdea
Kial Jinnah <kialj876@gmail.com>
2025-07-14 16:41:13 -0400

    playing with ci
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- packages/base-ui/tests/e2e/example.spec.ts
+++ packages/base-ui/tests/e2e/example.spec.ts
@@ -4,6 +4,6 @@ test.describe('Example', () => {
   test('Base page loads with expected visuals', async ({ page }) => {
     await page.goto('./')
     await expect(page.getByRole('heading', { name: 'Hello World! Base!' })).toBeVisible()
-    await expect(page.getByText('{ "name": "Hello from Nuxt layer" }')).toBeVisible()
+    await expect(page.getByText('{ "name": "Hello from Nuxt layer NEW AGAIN" }')).toBeVisible()
   })
 })
