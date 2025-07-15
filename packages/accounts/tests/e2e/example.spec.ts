97a4e91dae6380331faa73932e006f0c4e82af4d
Kial Jinnah <kialj876@gmail.com>
2025-07-15 11:46:47 -0400

    test breaking changes in base is picked up by accounts ci
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- packages/accounts/tests/e2e/example.spec.ts
+++ packages/accounts/tests/e2e/example.spec.ts
@@ -4,6 +4,5 @@ test.describe('Example', () => {
   test('Base page loads with expected visuals', async ({ page }) => {
     await page.goto('./')
     await expect(page.getByRole('heading', { name: 'Hello World! Base!' })).toBeVisible()
-    await expect(page.getByText('{ "name": "My amazing Nuxt layer **ACCOUNTS** playground" }')).toBeVisible()
   })
 })
--- packages/accounts/tests/e2e/example2.spec.ts
+++ packages/accounts/tests/e2e/example2.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test'
 test.describe('Example 2', () => {
   test('Base page loads with expected visuals', async ({ page }) => {
     await page.goto('./')
-    await expect(page.getByRole('heading', { name: 'Hello World! Base!' })).toBeVisible()
     await expect(page.getByText('{ "name": "My amazing Nuxt layer **ACCOUNTS**" }')).toBeVisible()
   })
 })
