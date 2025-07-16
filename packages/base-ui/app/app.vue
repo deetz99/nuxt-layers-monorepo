52e052f97ae30d3dc479b41ac5fd885d41206233
deetz99 <dietrich@wolpert.ca>
2025-07-16 12:52:55 -0700

    updaet app.vue text

--- packages/base-ui/app/app.vue
+++ packages/base-ui/app/app.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col lg:flex-row lg:gap-6 grow">
         <div class="grow max-w-full overflow-hidden">
           <HelloWorld />
-          Base layer text
+          Base layer text update
         </div>
       </div>
     </div>
