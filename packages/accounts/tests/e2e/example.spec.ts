95ce824636efb1da520da7651bc91c4e646cd757
Kial Jinnah <kialj876@gmail.com>
2025-07-16 10:28:19 -0400

    attempt playwright phone devices fix
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- packages/accounts/tests/e2e/example.spec.ts
+++ packages/accounts/tests/e2e/example.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test'
 test.describe('Example', () => {
   test('Base page loads with expected visuals', async ({ page }) => {
     await page.goto('./')
-    await expect(page.getByRole('heading', { name: 'Hello World! Base!' })).toBeVisible()
+    await expect(page.getByRole('heading', { name: 'Hello World!' })).toBeVisible()
   })
 })
