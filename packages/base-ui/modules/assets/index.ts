1c3b1ba842871d1e09161814ba7b5df7027a8a19
deetz99 <dietrich@wolpert.ca>
2025-07-17 17:36:02 -0700

    move back to assets module

---
+++ packages/base-ui/modules/assets/index.ts
@@ -0,0 +1,24 @@
+import { defineNuxtModule, createResolver, installModule } from '@nuxt/kit'
+
+// Module options TypeScript interface definition
+export interface ModuleOptions {
+  someconfig: boolean
+}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'assets',
+    configKey: 'assets'
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  async setup(_options, _nuxt) {
+    console.info('setting up assets module')
+    const resolver = createResolver(import.meta.url)
+
+    // _nuxt.options.css.push(resolver.resolve('./runtime/assets/core-main.css'))
+    _nuxt.options.css.push(resolver.resolve('./runtime/assets/core-tw.css'))
+    _nuxt.options.css.push(resolver.resolve('./runtime/assets/core-layout.css'))
+    await installModule('@nuxt/ui')
+  }
+})
--- packages/base-ui/nuxt.config.ts
+++ packages/base-ui/nuxt.config.ts
@@ -15,14 +15,21 @@ export default defineNuxtConfig({
     'BCGovLogoSmFr': resolve('./public/BCGovLogo/gov_bc_logo_vert_fr.png'),
     'BCGovLogoLgEn': resolve('./public/BCGovLogo/gov_bc_logo_horiz_en.png'),
     'BCGovLogoLgFr': resolve('./public/BCGovLogo/gov_bc_logo_horiz_fr.png'),
-    '#base': resolve('./'),
-    '#base-css': resolve('./app/assets/css/core-main.css')
+    '#base': resolve('./')
+    // '#base-css': resolve('./app/assets/css')
   },
 
   css: [
-    resolve('./app/assets/css/core-main.css')
-    // resolve('./app/assets/css/core-layout.css')
+    // resolve('./app/assets/css/core-main.css')
+    // resolve('./app/assets/css/core-layout.css'),
+    // resolve('./app/assets/css/core-tw.css')
   ],
 
   compatibilityDate: '2025-07-10'
+
+  // mayb euse this in package.json
+  // "exports": {
+  //   "./style.css": "./app/assets/css/core-main.css",
+  //   ".": "./nuxt.config.ts"
+  // },
 })
